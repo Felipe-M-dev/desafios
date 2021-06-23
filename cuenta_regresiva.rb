2316748d89c8e7ae09c1a747f89a7198d15523cc
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2021-06-23 17:55:46 -0400

    Update cuenta_regresiva.rb

--- cuenta_regresiva.rb
+++ cuenta_regresiva.rb
@@ -1,7 +1,15 @@
+#puts 'Ingrese un número para comenzar la cuenta:'
+#cuenta_regresiva = ARGV[0].to_i
+#puts "Contando desde #{cuenta_regresiva}..."
+#until cuenta_regresiva < 0
+#puts cuenta_regresiva
+#cuenta_regresiva -= 1
+#end
+
 puts 'Ingrese un número para comenzar la cuenta:'
 cuenta_regresiva = ARGV[0].to_i
 puts "Contando desde #{cuenta_regresiva}..."
-until cuenta_regresiva < 0
-puts cuenta_regresiva
-cuenta_regresiva -= 1
+while cuenta_regresiva >= 0
+    puts cuenta_regresiva
+    cuenta_regresiva -=1
 end
