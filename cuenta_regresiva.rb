ba6ababf68a6686e2f8dc108a996645cabbfefe3
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2021-06-23 18:14:02 -0400

    Update cuenta_regresiva.rb

--- cuenta_regresiva.rb
+++ cuenta_regresiva.rb
@@ -11,5 +11,5 @@ cuenta_regresiva = ARGV[0].to_i
 puts "Contando desde #{cuenta_regresiva}..."
 while cuenta_regresiva >= 0
     puts cuenta_regresiva
-    cuenta_regresiva -=1
+    cuenta_regresiva -= 1
 end
