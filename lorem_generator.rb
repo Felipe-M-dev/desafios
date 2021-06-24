e814527b561928f0f3d83142599cf248c5577f8a
Felipe-M-dev <39914824+Felipe-M-dev@users.noreply.github.com>
2021-06-23 21:01:06 -0400

    Create lorem_generator.rb

---
+++ lorem_generator.rb
@@ -0,0 +1,4 @@
+num = ARGV[0].to_i
+for i in (1..num)
+    puts "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis malesuada nunc. Etiam nec varius magna. Nulla erat arcu, faucibus eget mi non, pulvinar eleifend elit. Aenean scelerisque ultrices elit, id aliquam augue porttitor eget. Sed tempus accumsan purus sed viverra. Morbi euismod, ex non posuere efficitur, arcu turpis tempor dolor, sit amet eleifend dolor elit quis lorem. Donec auctor congue nibh sed porttitor. Nulla vehicula imperdiet diam vitae feugiat. Vivamus in mi a lacus euismod blandit. Donec fringilla ullamcorper gravida. In semper molestie mauris, eget consectetur velit ullamcorper eu.\n"
+end
